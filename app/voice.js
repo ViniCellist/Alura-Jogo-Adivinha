caa426b1b3add2f4c18bb80cc9bde91cbcd59e61
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-29 15:54:40 -0300

    feat: adding ;

--- app/voice.js
+++ app/voice.js
@@ -1,25 +1,25 @@
-const elementGuess = document.getElementById('chute')
+const elementGuess = document.getElementById('chute');
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-recognition.lang = 'pt-Br'
-recognition.start()
+recognition.lang = 'pt-Br';
+recognition.start();
 
-recognition.addEventListener('result', onSpeak)
+recognition.addEventListener('result', onSpeak);
 
 function onSpeak(e) {
-    let guess = e.results[0][0].transcript
-    displayGuess(guess)
-    verify(guess)
-}
+    let guess = e.results[0][0].transcript;
+    displayGuess(guess);
+    verify(guess);
+};
 
 function displayGuess(guess) {
     elementGuess.innerHTML = `
         <div>Você disse</div>
         <span class="box">${guess}</span>
         `
-}
+};
 
 recognition.addEventListener('end', () => {
-    recognition.start()
-})
+    recognition.start();
+});
