caaf5a7cb3f2cfc5b1328b5de99b6c9956f6c859
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-29 15:54:34 -0300

    feat: adding ;

--- app/random.js
+++ app/random.js
@@ -1,13 +1,13 @@
-const lowerValue = 1
-const higherValue = 1000
-const secretNumber = generateRandomNumber()
+const lowerValue = 1;
+const higherValue = 1000;
+const secretNumber = generateRandomNumber();
 
 function generateRandomNumber() {
-    return parseInt(Math.random() * higherValue + 1)
-}
+    return parseInt(Math.random() * higherValue + 1);
+};
 
-const elementLowerValue = document.getElementById('less-value')
-elementLowerValue.innerHTML = lowerValue
+const elementLowerValue = document.getElementById('less-value');
+elementLowerValue.innerHTML = lowerValue;
 
-const elementHigherValue = document.getElementById('greater-value')
-elementHigherValue.innerHTML = higherValue
+const elementHigherValue = document.getElementById('greater-value');
+elementHigherValue.innerHTML = higherValue;
